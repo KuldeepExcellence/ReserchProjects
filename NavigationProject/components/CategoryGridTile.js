abdfd8464028979d5fa3b7c8c144f07754df762f
Kuldeep Sharma <kuldeep.excellence@gmail.com>
2022-08-25 16:18:46 +0530

    creating and designing flatlist from the dammy data

---
+++ NavigationProject/components/CategoryGridTile.js
@@ -0,0 +1,45 @@
+import { Pressable, View, Text, StyleSheet } from "react-native"
+
+
+function CategoryGridTile({ title, color }) {
+    return <View style={styles.gridItem}>
+        <Pressable style={styles.button}>
+            <View style={styles.innerContainer}>
+                <Text>
+                    {title}
+                </Text>
+            </View>
+        </Pressable>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8
+
+    },
+
+    button: {
+        flex: 1
+
+    },
+
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: "center",
+        alightItems: "center"
+    }
+
+})
+
+export default CategoryGridTile
