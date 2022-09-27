10acd53d3e9cda77f5e03224f647f5e3bd626e67
Kuldeep Sharma <kuldeep.excellence@gmail.com>
2022-09-27 16:37:12 +0530

    new projectd added

---
+++ imagepicker/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+
+
+export default function App() {
+  return (
+    <View>
+      <Text> hello world  </Text>
+    </View>
+  )
+}
