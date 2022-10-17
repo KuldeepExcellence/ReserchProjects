7cf9c8c7c5c435d422c04338f7d42dec7918e17d
Kuldeep Sharma <kuldeep.excellence@gmail.com>
2022-10-17 14:07:12 +0530

    react native image picker done

--- croppickertest/App.js
+++ croppickertest/App.js
@@ -1,112 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, PermissionsAndroid } from "react-native";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const Camera = () => {
+  const [cameraPhoto, setCameraPhoto] = useState();
+  const [galleryPhoto, setGalleryPhoto] = useState();
+  let options = {
+    saveToPhotos: true,
+    mediaType: "photo",
+  }
+  const openCamera = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,);
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      const result = await launchCamera(options)
+      setCameraPhoto(result.assets[0].uri)
+    }
   };
 
+
+
+
+
+
+  const openGallery = async () => {
+    const result = await launchImageLibrary(options)
+    setGalleryPhoto(result.assets[0].uri);
+
+  };
+
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    <View >
+      <TouchableOpacity onPress={openCamera} >
+        <Text> Open Camera </Text>
+      </TouchableOpacity>
+      <Image style={{ height: 100, width: 100 }} source={{ uri: cameraPhoto }}></Image>
+      <TouchableOpacity onPress={openGallery} >
+        <Text > Open Gallery </Text>
+      </TouchableOpacity>
+      <Image style={{ height: 100, width: 100 }} source={{ uri: galleryPhoto }}></Image>
+    </View>
+  )
+}
+export default function App() {
+  return (
+    <View><Camera></Camera></View>
+  )
+}  
